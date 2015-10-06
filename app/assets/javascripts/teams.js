e4d9edbc39830e344b1a3a9cffed172b573655c3
reecebisel <reecebisel@gmail.com>
2015-10-06 16:57:53 -0600

    For loop to grab all pokeNames from API

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -4,16 +4,20 @@
 
 
 $(document).ready(function(){                                         // readies the document before running jquery or javascript functions
-
-  $.ajax("http://pokeapi.co/api/v1/pokemon", {                        // ajax function gets all pokemon from the API
-    type: "GET",
-    success: function(data){
-      var pokeNames = data.national_id                                // need help parseing the JSON response to grab needed data.
-      console.log(pokeNames);                                         // Ask Brad, either Jake or possibly Dave
-    }, 
-    error: function(data){ 
-      console.log("oh-nos!");
-    }
-  });    //$.ajax end
-
+  for (i= 1; i < 152; i++) {                                          // for loop to iterate through pokeIds in api
+    $.ajax("http://pokeapi.co/api/v1/pokemon/" + i, {                 // ajax function gets all pokemon from the API
+      type: "GET",                                                    // http action type through api
+      success: function(data){                                        
+        var $pokeName = data.name                                      // read Json object data and finds each pokemon name  
+        var $list = $('#poke_list');
+        $list.appendTo('<li>' + $pokeName + '</li>');
+        console.log($pokeName);
+      }, 
+      error: function(data){ 
+        console.log("oh-nos!");
+      }
+    });    //$.ajax end
+  }
 });  // document.ready end
+
+
