ca802f39c537da3c95b11f1e51cf28cb57ae4bfc
reecebisel <reecebisel@gmail.com>
2015-10-08 17:17:39 -0600

    ajax call now displays names of pokemon and the first type they possess.

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -3,21 +3,32 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-$(document).ready(function(){                                         // readies the document before running jquery or javascript functions
-  for (i= 1; i < 152; i++) {                                          // for loop to iterate through pokeIds in api
-    $.ajax("http://pokeapi.co/api/v1/pokemon/" + i, {                 // ajax function gets all pokemon from the API
-      type: "GET",                                                    // http action type through api
+$(document).ready(function(){                                                                   // readies the document before running jquery or javascript functions
+  
+  for (i= 1; i < 152; i++) {                                                                    // for loop to iterate through pokeIds in api
+    $.ajax("http://pokeapi.co/api/v1/pokemon/" + i, {                                           // ajax function gets all pokemon from the API
+      type: "GET",                                                                              // http action type through api
       success: function(data){                                        
-        var $pokeName = data.name                                      // read Json object data and finds each pokemon name  
-        var $list = $('#poke_list');
-        $list.appendTo('<li>' + $pokeName + '</li>');
-        console.log($pokeName);
+        var $pokeName = data.name;                                                              // Grabs the Pokemon name from list and displays them in the ul element in the teams#new.html.erb  
+        var $pokeTypes = data.types
+        var $pokeList = $('#poke_list');
+        $('<li class= "poke-list-data">' + 
+          $pokeName + 
+          " " +
+          '<span class="poke-type-data">' +
+          $pokeTypes[0].name +                                                                 // accesses the first type of the pokeTypes array and calls the name
+          '</span>' +
+          " " +
+          '</li>').appendTo($pokeList);                                                        // list element is being appended to the poke_list in the html. 
       }, 
-      error: function(data){ 
-        console.log("oh-nos!");
+      error: function(data){                                                                    
+        console.log("oh-nos!");                                                                // console logs oh-nos if there is an error 
       }
     });    //$.ajax end
-  }
-});  // document.ready end
+  }       // for loop end
+
+
+
+});  // document.ready end  
 
 
